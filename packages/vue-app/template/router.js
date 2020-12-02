09d3cfb0fede31933c31064f0bb9fe6a075da883
Pooya Parsa <pyapar@gmail.com>
2020-12-02 18:36:55 +0100

    fix: revert router override

--- packages/vue-app/template/router.js
+++ packages/vue-app/template/router.js
@@ -106,14 +106,5 @@ export const routerOptions = {
 }
 
 export function createRouter () {
-  const router = new Router(routerOptions)
-  // const resolve = router.resolve.bind(router)
-  // encodeURI(decodeURI()) ~> support both encoded and non-encoded urls
-  // router.resolve = (to, current, append) => {
-  //   if (typeof to === 'string') {
-  //     to = encodeURI(decodeURI(to))
-  //   }
-  //   return resolve(to, current, append)
-  // }
-  return router
+  return new Router(routerOptions)
 }
